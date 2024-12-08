d75bc3fadb306ef71416f1ce4918afc3077a2762
MrKermit <olafisz.pl@gmail.com>
2024-12-08 22:27:25 +0100

    Dodaj pliki projektów.

---
+++ FantasyCommander.cpp
@@ -0,0 +1,20 @@
+﻿#include <raylib.h>
+#include "world/world.h"
+#include <iostream>
+
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 600;
+
+int main()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Test");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+}
