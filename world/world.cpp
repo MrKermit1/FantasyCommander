620f89f584149e15ffd2941254b759690350ed1b
MrKermit <olafisz.pl@gmail.com>
2024-12-08 23:01:14 +0100

    added World class

--- world/world.cpp
+++ world/world.cpp
@@ -1,1 +1,19 @@
 #include "world.h"
+World::World(){}
+
+World::World(int worldMap[32][24], std::string worldName)
+{
+	initMap(worldMap);
+	this->name = worldName;
+}
+
+void World::initMap(int mapBuffer[32][24])
+{
+	for (int i = 0; i < 32; i++)
+	{
+		for (int j = 0; j < 24; j++)
+		{
+			map[i][j] = mapBuffer[i][j];
+		}
+	}
+}
--- world/world.h
+++ world/world.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <iostream>
 class World
 {
+private:
+	std::string name;
+	int map[32][24];
+
+	void initMap(int mapBuffer[32][24]);
+
+public:
+	World();
+	World(int mapBuffer[32][24], std::string worldName);
 };
 
