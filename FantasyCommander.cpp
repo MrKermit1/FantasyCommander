35b46e45096c98255560a64dd35bc8351172018e
MrKermit <olafisz.pl@gmail.com>
2025-07-15 23:14:18 +0200

    moved world and player to globals

--- FantasyCommander.cpp
+++ FantasyCommander.cpp
@@ -2,23 +2,20 @@
 #include <vector>
 #include <string>
 #include "raylib.h"
-#include "world.h"
-#include "player.h"
+#include "globals.h"
 #include "ui.h"
 const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 1000;
-Player player = Player();
-UI ui = UI(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 int main()
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Fantasy Commander");
     SetTargetFPS(60);
-    World world = World(&player);
-
+    UI ui = UI(SCREEN_WIDTH, SCREEN_HEIGHT);
     Creature c = Creature({ 120.0f, 100.0f }, Creature::WARRIOR, Creature::DWARF);
     Creature c2 = Creature({ 150.0f, 100.0f }, Creature::WARRIOR, Creature::DWARF);
+
 	world.AddCreature(c);
+	world.AddCreature(c2);
 
     while (!WindowShouldClose())
     {
@@ -28,7 +25,7 @@ int main()
         world.Update();
         world.Move();
         world.Draw();
-        ui.DrawResources(&player);
+        ui.DrawResources();
         ui.DrawShop();
 
         EndDrawing();
